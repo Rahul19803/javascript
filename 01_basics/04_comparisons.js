fcf2c9cd25b21d05adc4a62b0a06fe83e4b5cc8e
Rahul19803 <138676571+Rahul19803@users.noreply.github.com>
2024-07-08 19:46:21 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+/*console.log("2">1); // true
+console.log("02">1); //true */
+
+/*
+console.log(null > 0); //false
+console.log(null < 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0);   //true 
+ */
+
+
+//undefined ke case mai saare false aayenge
+
+// === -> strict check (abhi yeh khali conversion nahi karega abhi yeh datatype bhi check karega)
+console.log(2===2); //true
+console.log("2"===2); //false qki abhi khali convert nahi abhi datatype bhi check akr raha hai
