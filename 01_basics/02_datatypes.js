4bedd987c2a525364db47c24cff79f9e1d9b8956
Rahul19803 <138676571+Rahul19803@users.noreply.github.com>
2024-07-08 15:27:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name="Rahul"
+let age = 18
+let isLoggedIn=false
+
+// datatypes are as follows
+ 
+  //number range is=> 2 to the power of 53
+  //bigInt = for large numbers
+  //string=""
+  //boolean="true/false"
+  //null = standalone value hai
+  //undefined
+  //symbol= uniqueness
+
+  
