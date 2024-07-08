93246e2dd4de2af2d495815f8d71b2176e88ea16
Rahul19803 <138676571+Rahul19803@users.noreply.github.com>
2024-07-08 18:55:21 +0000

    conversion operation

---
+++ 01_basics/03_conversionoOperation.js
@@ -0,0 +1,22 @@
+let score=false
+
+console.log(typeof(score));
+
+let isNumber = Number(score)
+console.log(typeof(isNumber))
+console.log(isNumber)
+
+
+// "33"=>33
+// "33abc"=> NaN
+// true=> 1
+
+let isloggedIn=""
+
+let booleanisloggedIn=Boolean(isloggedIn)
+console.log(booleanisloggedIn);
+
+// 1=> true
+// 0=> false
+// "Rahul"=>true
+//""=> false
