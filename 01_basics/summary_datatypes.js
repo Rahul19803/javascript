24797c052d32318ca0a44b057d0be1a0165bcc52
Rahul19803 <138676571+Rahul19803@users.noreply.github.com>
2024-07-09 10:08:19 +0000

    summary of datatypes

---
+++ 01_basics/summary_datatypes.js
@@ -0,0 +1,18 @@
+//JavaScript is a dynamically typed language. This means that the type of a variable is determined at runtime, rather than at compile time.
+
+// #primitive
+
+/* there are 7 changes :1)String
+                        2)Number
+                        3)Boolean
+                        4)null
+                        5)undefined
+                        6)symbol-> for uniqueness
+                        7)BigInt
+*/
+
+// #Reference type(Non-Primitive)-> meaning memory mai inka direct refernece direvtly allocate kiya jaat hai 
+
+/* 1) Arrays
+   2) Objects
+   3) functions
