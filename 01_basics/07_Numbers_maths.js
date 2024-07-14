d62e2928a3fba66201a2a5fbe7dd6bb94bc9e524
Rahul19803 <138676571+Rahul19803@users.noreply.github.com>
2024-07-14 13:39:41 +0000

    math and num

--- 01_basics/07_Numbers_maths.js
+++ 01_basics/07_Numbers_maths.js
@@ -29,3 +29,22 @@ console.log(Math); //its an object
 console.log(Math.round(2.8));//-> 3
 console.log(Math.SQRT2);
 console.log(Math.abs(-3));
+console.log(Math.min(4,2,5,4,7,8,4,6));
+console.log(Math.max(2,4,5,3,7,5,45,32,4,8657,435));
+console.log(Math.ceil(8.2));//-> rounding off to its highest number which is 9
+console.log(Math.floor(8.2));//rounding of to its lowest number which is 8 //and many more functions
+
+//there is one more imp fucntion
+
+console.log(Math.random());// iski value hamesha 0 and 1 ke beech mai hi aayegi
+//abhi agar hume dice game banana hai for eg then tabhi hum math.random ko *10 kardenge toh decimal value ek position se right mai shift hojayega
+
+console.log(Math.random()*10);//-> 6 etc
+
+
+
+//for setting a range in math.random fucntion  
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))); //formula hai learn it but abhi bhi iska o/p 10 ke neeche upar hi aarha hai but hame 10 ke neeche nahi chaiye
+console.log(Math.floor(Math.random()*(max-min+1)+min));//-> above 10 random values
